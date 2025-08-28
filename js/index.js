bc8297f7714db815e71885210899d70ed2505945
Sayma-Shimu <ahmedsayma399@gmail.com>
2025-08-28 08:53:51 +0600

    card added

---
+++ js/index.js
@@ -0,0 +1,47 @@
+/*
+<div class=" border-3 border-red-600 card-1 bg-white rounded-xl p-8 shadow-lg ">
+
+                    <div class="flex justify-between items-center mb-4">
+                       <figure class="bg-[#ffe3e2] flex justify-center items-center rounded-2xl w-14 h-14">
+                         <img class="w-8 h-8 " src="./assets/fire-service.png" alt="fire-service">
+                       </figure>
+
+                        <i class="fa-regular fa-heart"></i>
+                    </div>
+
+                   <div class="mb-6">
+                        <h2 class="text-2xl font-bold">Fire Service Number</h2>
+                        <p class="text-lg font-normal text-[#5c5c5c]">Fire Service</p>
+                    </div>
+
+                    <!-- phone number----------- -->
+                    <div>
+                        <h2 class="text-3xl font-bold">999</h2>
+                        <p class="w-14 text-center p-1 bg-[#f2f2f2] rounded-lg text-[#5c5c5c]">Fire</p>
+                    </div>
+
+                    <!-- button----------- -->
+                    <div class="flex justify-evenly items-center gap-2 mt-6">
+                        <button class="w-1/2 text-center p-2 rounded-lg bg-[#ffffff] border-2 border-gray-300 text-[#5c5c5c]"><i class="fa-solid fa-copy"></i> Copy</button>
+                        <button class="w-1/2 p-2 text-center text-white rounded-lg bg-[#00a63e]" ><i class="fa-solid fa-phone"></i> Call</button>
+                    </div>
+                </div>
+                */
+
+
+
+
+
+
+
+
+             /*  
+               <!------------ call history--------------------- -->
+            
+                <aside class="col-span-12 md:col-span-3 bg-white p-4 rounded-lg mt-6 md:mt-0">
+
+                    <p class="font-medium text-xl">Call History</p>
+                    <button class="bg-[#00a63e] rounded-2xl">Clear</button>
+                </aside>
+
+                */
